ae2b01a0023842d54225357a65f1546a89d60dcc
David Pla Montero <dpla@foreworth.com>
2023-04-17 10:29:03 +0200

    Change

--- super_mega_file.js
+++ super_mega_file.js
@@ -1,13 +1,6 @@
 let xMax = canvas.height;
 let slope = 1.2;
 let intercept = 70;
-// Line Function
-function f(x) {
-  return x * slope + intercept;
-}let xMax = canvas.height;
-let slope = 1.2;
-let intercept = 70;
-
 // Plot Scatter
 ctx.moveTo(0, intercept);
 ctx.lineTo(xMax, f(xMax));
